405439f3ade1abcb0e922283a819b15879e47a3d
Higor Nogueira <higorcesarqn@gmail.com>
2018-03-24 18:15:42 -0300

    first commit

---
+++ src/AspNetCoreAuth2/Pages/Account/Login.cshtml.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace AspNetCoreAuth2.Pages.Account
+{
+    public class LoginModel : PageModel
+    {
+        [BindProperty]
+        public LoginInputModel LoginInputModel {get;set;}
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            if (!IsUserAuthenticated(LoginInputModel.UserName, LoginInputModel.Password))
+            {
+                ModelState.AddModelError(string.Empty, "Usúario ou senha inválidos");
+                return Page();
+            }
+
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, LoginInputModel.UserName)
+            };
+
+            var userIdentity = new ClaimsIdentity(claims, "login");
+            var principal = new ClaimsPrincipal(userIdentity);
+
+            await HttpContext.SignInAsync(principal);
+            return Redirect("/");
+        }
+
+        public async Task<IActionResult> OnPostLogout()
+        {
+            await HttpContext.SignOutAsync();
+            return Redirect("/");
+        }
+
+        private bool IsUserAuthenticated(string userName, string password)
+        {
+            //simulação de autenticação no banco de dados.
+            return true;
+        }
+    }
+}
---
+++ src/AspNetCoreAuth2/Pages/Account/LoginInputModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspNetCoreAuth2.Pages.Account
+{
+    public class LoginInputModel
+    {
+        [Required]
+        [MinLength(5)]
+        public string UserName { get; set; }
+        [Required]
+        [MinLength(5)]
+        public string Password { get; set; }
+    }
+}
